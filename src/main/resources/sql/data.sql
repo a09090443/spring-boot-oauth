aef7e1a81673eaed8a188c5e460958cdfc1018f6
Gary <zipe.daden@gmail.com>
2018-04-10 17:49:17 +0800

    Add column and change password.

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -8,9 +8,9 @@ INSERT INTO oauth_client_details
 (client_id, resource_ids, client_secret, `scope`, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove)
 VALUES('test1', NULL, 'secret', 'user_info', 'authorization_code,refresh_token', 'http://www.baidu.com', 'ROLE_USER', 1800, 86400, NULL, 'true');
 
-INSERT INTO user_info
-(user_id, login_id, password, title_id, first_name, last_name, email, address, birthday, image, phone, activated, register_time)
-VALUES('000001', 'admin', 'b8f57d6d6ec0a60dfe2e20182d4615b12e321cad9e2979e0b9f81e0d6eda78ad9b6dcfe53e4e22d1', '1', '最高', '指揮官', 'admin@localhost.com', '天堂路 001 號', '1992-01-01', '000001.jpg', '1234-567890', 1, NULL);
+INSERT INTO dev_network.user_info
+(user_id, login_id, password, first_name, last_name, email, address, birthday, image, phone, activated, register_time)
+VALUES('000001', 'admin', '$2a$10$edhXzouCrFzV/8WhlT4pn.V.fro7J4rvAMFfjpFTxHmQj7.N4meDa', '&#26368;&#39640;', '&#25351;&#25582;&#23448;', 'admin@localhost.com', '&#22825;&#22530;&#36335; 001 &#34399;', '1992-01-01', '000001.jpg', '1234567890', 1, '2017-12-31 02:30:49');
 
 INSERT INTO authority
 (authority_id, name)
